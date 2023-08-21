e69f3213f91f5a4c8527f7532610a4c13e29475b
Muhammadali <a@a.com>
2023-08-21 13:24:27 +0500

    add files

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function send(){
     let name = document.getElementById('email').value;
     let phone = document.getElementById('text').value;
   
-    sendtelegram(name  + ' Saytda  ' + phone + '  Deb yozdi');
+    sendtelegram(name  + ' в сайте  ' + phone + '   написал(а) ');   
     document.getElementById('email').value = '';
     document.getElementById('text').value = '';
 };
-function sendtelegram(message) { let telegram_bot_id = "6537391165:AAFO2FXWFBKt9waUnf77b4d9TBc5gBWTKgg"; let chat_id = 6140878431; let settings = { "async": true, "crossDomain": true, "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage", "method": "POST", "headers": { "Content-Type": "application/json", "cache-control": "no-cache" }, "data": JSON.stringify({ "chat_id": chat_id, "text": message }) }; $.ajax(settings).done(function (response) { }); };
+function sendtelegram(message) { let telegram_bot_id = "6509898610:AAEtyzcgascobyv2e--ZM_lpqNdhBTqsDEs"; let chat_id = 5887303814; let settings = { "async": true, "crossDomain": true, "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage", "method": "POST", "headers": { "Content-Type": "application/json", "cache-control": "no-cache" }, "data": JSON.stringify({ "chat_id": chat_id, "text": message }) }; $.ajax(settings).done(function (response) { }); };
